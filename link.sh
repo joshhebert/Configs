aa317e0d19d1c24687e1648218e6c696dba5768e
Josh Hebert <jahebert@wpi.edu>
2016-02-15 21:33:24 -0500

    Extra crud

--- link.sh
+++ link.sh
@@ -1,35 +1,32 @@
 cd ~
 echo "Assuming config directory is ~/configs/..."
 
-rm -r .i3
+rm -rf .i3
 ln -s configs/.i3 		            .i3
 ln -s configs/.i3/.i3blocks.conf    .i3/.i3blocks.conf
 echo "i3 config linked..."
 
-rm -r .vim .vimrc
+rm -rf .vim .vimrc
 ln -s configs/.vim 		            .vim
 ln -s configs/.vimrc 		        .vimrc
 echo "vim config linked..."
 
-rm -r .zsh .zshrc
+rm -rf .zsh .zshrc
 ln -s configs/.zsh 		            .zsh
 ln -s configs/.zshrc 		        .zshrc
 echo "zsh config linked..."
 
-rm -r .tmux.conf
+rm -rf .tmux.conf
 ln -s configs/.tmux.conf 	        .tmux.conf
 echo "tmux config linked..."
 
-rm -r .Xdefaults
+rm -rf .Xdefaults
 ln -s configs/.Xdefaults 	        .Xdefaults
 echo "Xdefaults linked..."
 
-rm -r .dir_colors
+rm -rf .dir_colors
 ln -s configs/.dir_colors 	        .dir_colors
 echo "dir_colors linked..."
 
-rm -r .scm_breeze
-ln -s configs/scm_breeze            .scm_breeze
-echo "scm_breeze linked..."
 
 echo "done"
